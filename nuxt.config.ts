372e519cb658b0ae5955793b9dccc5fa1cd651a3
Aidan Stevens <aidan@lookedafter.com>
2024-02-23 16:55:58 +1100

    Fix nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/kinde',
-    '@nuxtjs/tailwindcss',
     '@nuxt/ui'
   ],
   kinde: {
-    authDomain: 'https://<your_kinde_subdomain>.kinde.com',
-    clientId: '<your_kinde_client_id>',
     // middleware to your Nuxt application.
     // 
     // middleware: false,
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLink to="/api/login" external>
+      Sign in
+    </NuxtLink>
+
+    <NuxtLink to="/api/register" external>
+      Sign up
+    </NuxtLink>
+
+    <h1 class="text-xl">Home</h1>
+  </div>
+</template>
